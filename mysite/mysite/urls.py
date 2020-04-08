374adcea4e88e7e8824cb696bd89e5cba2702d20
Mara Hart <maralihart@gmail.com>
2020-04-07 21:53:18 -0400

    Write views that actually do something

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name = 'index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
