346cd9d26d737629b36ba9f1245cf9af328bf1fa
Mara Hart <maralihart@gmail.com>
2020-04-23 16:27:29 -0400

    Completed

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -7,17 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-# application = get_wsgi_application()
-
 import os
-import sys
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
+
+# import os
+# import sys
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
